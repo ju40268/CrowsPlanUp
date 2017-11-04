211db9f87812312341c1d515d189db2a3c9840a7
may811204 <ju40268@gmail.com>
2017-11-04 18:33:42 -0500

    ionic setup & my Group

---
+++ src/pages/mygroup/mygroup.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-mygroup',
+  templateUrl: 'mygroup.html'
+})
+export class MyGroupPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
